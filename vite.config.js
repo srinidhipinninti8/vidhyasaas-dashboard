6274c11c0912ea18b8f8ab9dfa8870afe4c4b623
srinidhipinninti8 <srinidhipinninti8@gmail.com>
2026-03-28 07:03:07 +0530

    Fix: Redirect build to public folder for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // This ensures the output folder is named 'dist'
+    outDir: 'public', // Change this from 'dist' to 'public'
   }
 })
